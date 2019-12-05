92158d91e59bb0ce4b8f30e929ba6ffa6a926b57
zydLin <zdl3gk@virginia.edu>
2019-12-05 11:30:32 -0500

    major bug: changed how getSpec retrieves the data

--- zylutils/set_dustspec.py
+++ zylutils/set_dustspec.py
@@ -27,72 +27,76 @@ class dustspec(object):
         Parameters
         ----------
         specs        : list
-                       list containing the names of the species
+                       list containing the names of the species. order matters!
         """
         
+        nspecs = len(specs)
+        for ispec in specs:
+            # Waterice from Semenov
+            if ispec == 'Waterice':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/watericek.lnk', 
+                    'watericek', 
+                    'waterice', 
+                    0.92, 
+                    5.55)
 
-        # Waterice from Semenov
-        if 'Waterice' in specs:
-            self.lnkfiles.append('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/watericek.lnk')
-            self.optconst.append('watericek')
-            self.graintype.append('waterice')
-            self.swgt.append(0.92)
-            self.mabun.append(5.55)
 
-        # Waterice from Warren2008
-        if 'WarrenWater' in specs:
-            self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Warren/RefractiveIndex/waterice.lnk', 
-                'waterice',
-                'waterice', 
-                0.92,
-                5.55)
+            # Waterice from Warren2008
+            elif ispec == 'WarrenWater':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Warren/RefractiveIndex/waterice.lnk', 
+                    'waterice',
+                    'waterice', 
+                    0.92,
+                    5.55)
 
-        # organics from Semenov
-        if 'Organics' in specs:
-            self.lnkfiles.append('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/organicsk.lnk')
-            self.optconst.append('organicsk')
-            self.graintype.append('organics')
-            self.swgt.append(1.5)
-            self.mabun.append(3.53)
+            # organics from Semenov
+            elif ispec == 'Organics':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/organicsk.lnk', 
+                    'organicsk', 
+                    'organics', 
+                    1.5, 
+                    3.53)
 
-        # carbon from Jena (Jaeger) 
-        if 'Carbon' in specs:
-            self.lnkfiles.append('/home/zdl3gk/mainProjects/my_opacity/Jena/Carbon/cel400.lnk')
-            self.optconst.append('cel400.lnk')
-            self.graintype.append('carbon')
-            self.swgt.append(1.5)
-            self.mabun.append(3.53)
+            # carbon from Jena (Jaeger) 
+            elif ispec == 'Carbon':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Jena/Carbon/cel400.lnk',
+                    'cel400.lnk', 
+                    'carbon', 
+                    1.5, 
+                    3.53)
 
-        # organics from Henning 1996
-        if 'Henning_Organics' in specs:
-            self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Henning/RefractiveIndex/organicsk.lnk',
-                'organicsk',
-                'organics',
-                1.5,
-                3.53)
+            # organics from Henning 1996
+            elif ispec == 'Henning_Organics':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Henning/RefractiveIndex/organicsk.lnk',
+                    'organicsk',
+                    'organics',
+                    1.5,
+                    3.53)
 
-        # troilite from Henning 1996
-        if 'Henning_Troilite' in specs:
-            self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Henning/RefractiveIndex/troilitek.lnk',
-                'troilitek',
-                'troilite',
-                4.83,
-                0.65) #details from Birnstiel 2018
+            # troilite from Henning 1996
+            elif ispec == 'Henning_Troilite':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Henning/RefractiveIndex/troilitek.lnk',
+                    'troilitek',
+                    'troilite',
+                    4.83,
+                    0.65) #details from Birnstiel 2018
 
-        ## silicates from Semenov
-        if 'Sil_Sem' in specs:
-            self.lnkfiles.append('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/pyrmg70k.lnk')
-            self.optconst.append('pyrmg70')
-            self.graintype.append('silicate')
-            self.swgt.append(3.49)
-            self.mabun.append(2.64)
+            ## silicates from Semenov
+            elif ispec == 'Sil_Sem':
+                self.fnSpec('/home/zdl3gk/mainProjects/my_opacity/Semenov/RefractiveIndex/pyrmg70k.lnk', 
+                    'pyrmg70', 
+                    'silicate', 
+                    3.49, 
+                    2.64)
 
-        # silicates from Draine (from Dullemond Radmc3d)
-        if 'Sil_Draine' in specs:
-            self.fnSpec('/home/zdl3gk/coding/fortran/radmc3d_code/radmc3d-0.41/opac/dust_continuum/astrosil_draine03/astrosilicate_draine03.lnk', 
-                'astrosilicate_draine03',
-                'silicate', 
-                3.3, 
-                2.64
-                )
+            # silicates from Draine (from Dullemond Radmc3d)
+            elif ispec == 'Sil_Draine':
+                self.fnSpec('/home/zdl3gk/coding/fortran/radmc3d_code/radmc3d-0.41/opac/dust_continuum/astrosil_draine03/astrosilicate_draine03.lnk', 
+                    'astrosilicate_draine03',
+                    'silicate', 
+                    3.3, 
+                    2.64
+                    )
+            else:
+                raise ValueError('species not found in available data base: %s'%ispec)
 
